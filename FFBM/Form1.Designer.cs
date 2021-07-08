2acc4e563ea327d6ba6cce25a4d4a891fd958e44
Juan Manuel Ruiz Plascencia <Juan.Manuel.Plascencia@ibm.com>
2021-07-08 16:57:26 -0500

    first commit

---
+++ FFBM/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace FFBM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.preview = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DeviceMessageTextBlock = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.Button();
+            this.brBar = new System.Windows.Forms.TrackBar();
+            this.br = new System.Windows.Forms.Label();
+            this.contrast = new System.Windows.Forms.Label();
+            this.contrastBar = new System.Windows.Forms.TrackBar();
+            this.gamma = new System.Windows.Forms.Label();
+            this.gammaBar = new System.Windows.Forms.TrackBar();
+            this.saturation = new System.Windows.Forms.Label();
+            this.saturationBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contrastBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gammaBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saturationBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(919, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose device";
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(24, 40);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(873, 530);
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            this.pic.Click += new System.EventHandler(this.pic_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(922, 547);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(121, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Take Picture";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(922, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(264, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(922, 116);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(264, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(922, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Chose resolution";
+            // 
+            // preview
+            // 
+            this.preview.Location = new System.Drawing.Point(1065, 547);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(121, 23);
+            this.preview.TabIndex = 7;
+            this.preview.Text = "Preview";
+            this.preview.UseVisualStyleBackColor = true;
+            this.preview.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(922, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "AutoFocus";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(922, 464);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // DeviceMessageTextBlock
+            // 
+            this.DeviceMessageTextBlock.AutoSize = true;
+            this.DeviceMessageTextBlock.Location = new System.Drawing.Point(848, 575);
+            this.DeviceMessageTextBlock.Name = "DeviceMessageTextBlock";
+            this.DeviceMessageTextBlock.Size = new System.Drawing.Size(35, 13);
+            this.DeviceMessageTextBlock.TabIndex = 10;
+            this.DeviceMessageTextBlock.Text = "label3";
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(922, 504);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(264, 23);
+            this.stop.TabIndex = 11;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // brBar
+            // 
+            this.brBar.Location = new System.Drawing.Point(922, 167);
+            this.brBar.Name = "brBar";
+            this.brBar.Size = new System.Drawing.Size(264, 45);
+            this.brBar.TabIndex = 12;
+            // 
+            // br
+            // 
+            this.br.AutoSize = true;
+            this.br.Location = new System.Drawing.Point(922, 151);
+            this.br.Name = "br";
+            this.br.Size = new System.Drawing.Size(56, 13);
+            this.br.TabIndex = 13;
+            this.br.Text = "Brightness";
+            // 
+            // contrast
+            // 
+            this.contrast.AutoSize = true;
+            this.contrast.Location = new System.Drawing.Point(922, 199);
+            this.contrast.Name = "contrast";
+            this.contrast.Size = new System.Drawing.Size(46, 13);
+            this.contrast.TabIndex = 14;
+            this.contrast.Text = "Contrast";
+            // 
+            // contrastBar
+            // 
+            this.contrastBar.Location = new System.Drawing.Point(922, 215);
+            this.contrastBar.Name = "contrastBar";
+            this.contrastBar.Size = new System.Drawing.Size(264, 45);
+            this.contrastBar.TabIndex = 15;
+            // 
+            // gamma
+            // 
+            this.gamma.AutoSize = true;
+            this.gamma.Location = new System.Drawing.Point(922, 247);
+            this.gamma.Name = "gamma";
+            this.gamma.Size = new System.Drawing.Size(43, 13);
+            this.gamma.TabIndex = 16;
+            this.gamma.Text = "Gamma";
+            // 
+            // gammaBar
+            // 
+            this.gammaBar.Location = new System.Drawing.Point(922, 266);
+            this.gammaBar.Name = "gammaBar";
+            this.gammaBar.Size = new System.Drawing.Size(264, 45);
+            this.gammaBar.TabIndex = 17;
+            // 
+            // saturation
+            // 
+            this.saturation.AutoSize = true;
+            this.saturation.Location = new System.Drawing.Point(922, 298);
+            this.saturation.Name = "saturation";
+            this.saturation.Size = new System.Drawing.Size(55, 13);
+            this.saturation.TabIndex = 18;
+            this.saturation.Text = "Saturation";
+            // 
+            // saturationBar
+            // 
+            this.saturationBar.Location = new System.Drawing.Point(922, 317);
+            this.saturationBar.Name = "saturationBar";
+            this.saturationBar.Size = new System.Drawing.Size(264, 45);
+            this.saturationBar.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1219, 597);
+            this.Controls.Add(this.saturationBar);
+            this.Controls.Add(this.saturation);
+            this.Controls.Add(this.gammaBar);
+            this.Controls.Add(this.gamma);
+            this.Controls.Add(this.contrastBar);
+            this.Controls.Add(this.contrast);
+            this.Controls.Add(this.br);
+            this.Controls.Add(this.brBar);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.DeviceMessageTextBlock);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.preview);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contrastBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gammaBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saturationBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button preview;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label DeviceMessageTextBlock;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.TrackBar brBar;
+        private System.Windows.Forms.Label br;
+        private System.Windows.Forms.Label contrast;
+        private System.Windows.Forms.TrackBar contrastBar;
+        private System.Windows.Forms.Label gamma;
+        private System.Windows.Forms.TrackBar gammaBar;
+        private System.Windows.Forms.Label saturation;
+        private System.Windows.Forms.TrackBar saturationBar;
+    }
+}
+
---
+++ FFBM/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using System.Windows.Threading;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using com.ipevo.windows.CameraKit;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.IO;
+using com.ipevo.windows.ToolKit;
+using System.Drawing.Imaging;
+
+
+
+namespace FFBM
+{
+
+    public partial class Form1 : Form
+    {
+        private WriteableBitmap captureWriteableBitmap;
+        private ICCamera previousSelectedCamera = null;
+        List<Dictionary<ICCamera.FormatKey, object>> supportFormats = new List<Dictionary<ICCamera.FormatKey, object>>();
+
+        private ICCameraStreamProxy.StreamObserver streamObserver;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private ICCamera ActionDevice
+        {
+            get
+            {
+                ICCamera camera = null;
+                if (this.comboBox1.SelectedItem != null)
+                {
+                    foreach (ICCamera value in ICCameraManager.sharedManager.cameras)
+                    {
+                        if (value.CameraInstanceName == this.comboBox1.SelectedItem.ToString())
+                        {
+                            camera = value;
+                            break;
+                        }
+                    }
+                }
+                return camera;
+            }
+        }
+
+        private void UpdateDevice(string notificationName, object sender, object userInfo)
+        {
+            Dispatcher.CurrentDispatcher.InvokeAsync(new Action(() =>
+            {
+                switch (notificationName)
+                {
+                    case ICCameraManager.ICNotification.DeviceAttached:
+                        {
+                            if (!comboBox1.Items.Contains((userInfo as ICCamera).CameraInstanceName))
+                                comboBox1.Items.Add((userInfo as ICCamera).CameraInstanceName);
+                        }
+                        break;
+                    case ICCameraManager.ICNotification.DeviceDetached:
+                        {
+                            if (comboBox1.Items.Contains((userInfo as ICCamera).CameraInstanceName))
+                                comboBox1.Items.Remove((userInfo as ICCamera).CameraInstanceName);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }));
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            ICCameraManager.sharedManager.startMonitor();
+            comboBox1.DataSource = ICCameraManager.sharedManager.cameras;
+        }
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //var camera_selected = this.comboBox1.SelectedIndex.ToString();
+            ICCamera camera = comboBox1.SelectedItem as ICCamera;
+
+            foreach (var item in camera.supportedFormats())
+            {
+                comboBox2.Items.Add(item[ICCamera.FormatKey.FormatInfo]);
+            }
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void Window_Close(object sender, EventArgs e)
+        {
+            ICCameraManager.sharedManager.stopMonitor();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*Show a preview of taken photos*/
+            Form2 win2 = new Form2();
+            win2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ICCamera camera = this.ActionDevice;
+            if (camera != null)
+            {
+                camera.getAutoFocus(out bool isAuto);
+                if (!isAuto)
+                {
+                    camera.setAutoFocus(true);
+                }
+                camera.startFocus();
+            }
+        }
+
+        private void pic_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            /*Start streaming aftar that you choose the camera*/
+            ICCamera camera = comboBox1.SelectedItem as ICCamera;
+            bool isHasCapabilityAutoExposure = camera.hasCapability(ICCamera.Capability.AutoExposure);
+            NSNotificationCenter.defaultCenter.addNotificationObserver(ICCameraManager.ICNotification.DeviceFocusBegin, new
+            NSNotificationCenter.NotificationObserver(cameraFocusStatus));
+
+            List<Dictionary<ICCamera.FormatKey, object>> supportFormats = camera.supportedFormats();
+            Dictionary<ICCamera.FormatKey, object> setFormat = supportFormats[comboBox2.SelectedIndex];
+            camera.setFormat(setFormat);
+
+            ICCameraStreamProxy.StreamObserver streamObserver;
+            streamObserver = new ICCameraStreamProxy.StreamObserver(UpdateStream);
+            ICCameraStreamProxy.sharedProxy.addStreamObserver(camera, streamObserver);
+        }
+
+        private void cameraFocusStatus(string notificationName, object sender, object userInfo)
+        {
+            if (userInfo != null)
+            {
+                System.Collections.Hashtable data = userInfo as System.Collections.Hashtable;
+                ICCamera camera = data[ICCameraManager.ICCommonString.Camera] as ICCamera;
+                string focusStatus = notificationName ==
+                ICCameraManager.ICNotification.DeviceFocusBegin ? "Focus Start" : "FocusFinish";
+
+                Dispatcher.CurrentDispatcher.Invoke(new Action(() =>
+                {
+                    if (camera == comboBox1.SelectedItem as ICCamera)
+                        DeviceMessageTextBlock.Text = string.Format("{0} {1}",
+                        camera.CameraInstanceName, focusStatus);
+                }));
+
+            }
+        }
+
+
+        private void UpdateStream(ICCamera camera, IntPtr buffer, int bufferLength, int frameWidth = 0, int frameHeight = 0)
+        {
+            Dispatcher.CurrentDispatcher.Invoke(new System.Threading.ThreadStart(() =>
+            {
+                StreamBufferToWriteableBitmap(camera, buffer, bufferLength);
+            }));
+        }
+
+        [System.Runtime.InteropServices.DllImport("Kernel32.dll", EntryPoint = "RtlMoveMemory")]
+        private static extern void CopyMemory(IntPtr Destination, IntPtr Source, int Length);
+        object lockObject = new object();
+        private void StreamBufferToWriteableBitmap(ICCamera camera, IntPtr buffer, int bufferLength)
+        {
+            lock (lockObject)
+            {
+                if (captureWriteableBitmap == null)
+                {
+                    camera.getFormat(out Dictionary<ICCamera.FormatKey, object> formatInfo);
+                    if (formatInfo != null
+                        && formatInfo.Count > 0)
+                        this.CreateWriteableBitmap(Convert.ToInt32(formatInfo[ICCamera.FormatKey.Width]), Convert.ToInt32(formatInfo[ICCamera.FormatKey.Height]));
+                }
+                else
+                {
+                    int writeableBitmapSize = (int)captureWriteableBitmap.Width * (int)captureWriteableBitmap.Height * 4;
+                    if (writeableBitmapSize != bufferLength)
+                    {
+                        captureWriteableBitmap = null;
+
+                        camera.getFormat(out Dictionary<ICCamera.FormatKey, object> formatInfo);
+                        if (formatInfo != null
+                            && formatInfo.Count > 0)
+                            this.CreateWriteableBitmap(Convert.ToInt32(formatInfo[ICCamera.FormatKey.Width]), Convert.ToInt32(formatInfo[ICCamera.FormatKey.Height]));
+                    }
+
+                    if (captureWriteableBitmap != null
+                        && buffer != IntPtr.Zero)
+                    {
+                        try
+                        {
+                            captureWriteableBitmap.Lock();
+                            System.Windows.Int32Rect rect = new System.Windows.Int32Rect(0, 0, (int)captureWriteableBitmap.Width, (int)captureWriteableBitmap.Height);
+                            IntPtr backbuffer = captureWriteableBitmap.BackBuffer;
+                            CopyMemory(backbuffer, buffer, bufferLength);
+                            captureWriteableBitmap.AddDirtyRect(rect);
+                            captureWriteableBitmap.Unlock();
+                            pic.Image = BitmapFromWriteableBitmap(captureWriteableBitmap);
+                            
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Diagnostics.Debug.WriteLine(ex.ToString());
+
+                            captureWriteableBitmap = null;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void CreateWriteableBitmap(int sourceWidth, int sourceHeight)
+        {
+            try
+            {
+                captureWriteableBitmap = new WriteableBitmap(sourceWidth, sourceHeight, 96d, 96d, PixelFormats.Bgr32, null);
+                pic.Image = BitmapFromWriteableBitmap(captureWriteableBitmap);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        private Bitmap BitmapFromWriteableBitmap(WriteableBitmap source)
+        {
+            Bitmap bmp;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create((BitmapSource)source));
+                enc.Save(outStream);
+                bmp = new Bitmap(outStream);
+            }
+            bmp.RotateFlip(RotateFlipType.Rotate180FlipX);
+            return bmp;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ICCamera camera = ActionDevice;
+
+            if (camera != null && supportFormats.Count > 0)
+            {
+                ICCameraStreamProxy.sharedProxy.stopStreamObserver(camera);
+                Dictionary<ICCamera.FormatKey, object> setFormat = supportFormats[comboBox2.SelectedIndex];
+                camera.setFormat(setFormat);
+
+                ICCameraStreamProxy.sharedProxy.startStreamObserver(camera);
+            }
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            ICCamera camera = comboBox1.SelectedItem as ICCamera;
+            //Console.WriteLine(comboBox1.SelectedItem.ToString());
+            ICCameraStreamProxy.sharedProxy.stopStreamObserver(camera);
+            ICCameraStreamProxy.StreamObserver streamObserver;
+            streamObserver = new ICCameraStreamProxy.StreamObserver(UpdateStream);
+            ICCameraStreamProxy.sharedProxy.removeStreamObserver(camera, streamObserver);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            this.SaveCameraImage();
+        }
+
+
+
+        private void SaveCameraImage()
+        {
+            ICCamera camera = comboBox1.SelectedItem as ICCamera;
+            camera.getFormat(out Dictionary<ICCamera.FormatKey, object> formatInfo);
+            if (formatInfo != null && formatInfo.Count > 0)
+            {
+                var sourceWidth = Convert.ToInt32(formatInfo[ICCamera.FormatKey.Width]);
+                var sourceHeight = Convert.ToInt32(formatInfo[ICCamera.FormatKey.Height]);
+                captureWriteableBitmap = new WriteableBitmap(sourceWidth, sourceHeight, 96d, 96d, PixelFormats.Bgr32, null);
+                var frame = BitmapFromWriteableBitmap(captureWriteableBitmap);
+                frame.Save("test.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            }
+                
+          
+
+            /*System.Drawing.Bitmap captureBitmap = BitmapSourceToBitmap((BitmapSource)this.captureWriteableBitmap);
+            string fileName = AppDomain.CurrentDomain.BaseDirectory + DateTime.Now.ToString("MM-dd-yyyy_HHmmss") + ".jpg";
+            captureBitmap.RotateFlip(System.Drawing.RotateFlipType.Rotate180FlipX);
+            captureBitmap.Save(fileName);
+            captureBitmap?.Dispose();
+            captureBitmap = null;
+
+            //open current folder
+            System.Diagnostics.Process.Start(AppDomain.CurrentDomain.BaseDirectory);*/
+        }
+    }
+
+
+
+
+}
